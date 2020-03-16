eab27dbf4e3d9a14c17a16c3f83f93d9b3bd59f8
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-03-16 16:34:02 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundoParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source: BaseDatos\Parcial2.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          //  modelBuilder.Entity<Productos>().HasData(new Productos { ProductoId = 1, Descripcion = "Chocolate", Precio = 100 });
+
+            //modelBuilder.Entity<Productos>().HasData(new Productos { ProductoId = 2, Descripcion = "cafe", Precio = 100 });
+
+            //modelBuilder.Entity<Productos>().HasData(new Productos { ProductoId = 3, Descripcion = "arroz", Precio = 100 });
+
+
+        }
+    }
+}
